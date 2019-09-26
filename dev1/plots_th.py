dc6bd7f3981eb2204f965209a06eab162a2d93f6
Velythyl <charlie.gauthier@umontreal.ca>
2019-09-25 22:12:00 -0400

    4.a th

--- dev1/plots_th.py
+++ dev1/plots_th.py
@@ -8,26 +8,19 @@ x21 = [0, -10, -2]
 x12 = [5, 0, 5]
 x22 = [10, 5, 5]
 
-plt.plot(x11, x21, marker=".", linestyle = 'None')
-plt.plot(x12, x22, marker="v", linestyle = 'None')
-
-"""
 points = list(zip(x11, x21)) + list(zip(x12, x22))
 
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
 vor = Voronoi(points)
-fig = voronoi_plot_2d(vor)
+fig = voronoi_plot_2d(vor, show_points=False, show_vertices=False)
 
-for r in range(len(vor.point_region)):
-    region = vor.regions[vor.point_region[r]]
-
-    polygon = [vor.vertices[i] for i in region]
-    plt.fill(*zip(*polygon), color=[1, 1, 0, 1])
-"""
+plt.plot(x11, x21, marker=".", linestyle = 'None')
+plt.plot(x12, x22, marker="v", linestyle = 'None')
 
 plt.ylabel("Error rate")
 plt.xlabel("Value of h")
+
 plt.show()
 
 
